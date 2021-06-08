db1f97769420b70e4b6866375ad02ee086ef2763
Samuel Heal <samuelheal@hotmail.com>
2021-06-08 16:50:54 +1000

    basic set up working

---
+++ public/js/login.js
@@ -0,0 +1,69 @@
+const loginFormHandler = async (event) => {
+  event.preventDefault();
+
+  // Collect values from the login form
+  const email = document.querySelector('#email-login').value.trim();
+  const password = document.querySelector('#password-login').value.trim();
+
+  if (email && password) {
+    // Send a POST request to the API endpoint
+    const response = await fetch('/api/users/login', {
+      method: 'POST',
+      body: JSON.stringify({ email, password }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+      // If successful, redirect the browser to the profile page
+      document.location.replace('/dashboard');
+    } else {
+      alert("Incorrect email or password");
+    }
+  }
+};
+
+const signupFormHandler = async (event) => {
+  event.preventDefault();
+
+  const name = document.querySelector('#name-signup').value.trim();
+  const email = document.querySelector('#email-signup').value.trim();
+  const password = document.querySelector('#password-signup').value.trim();
+
+  if (name && email && password) {
+    const response = await fetch('/api/users', {
+      method: 'POST',
+      body: JSON.stringify({ name, email, password }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+      document.location.replace('/dashboard');
+    } else {
+      alert("Please check the details you have entered are correct and try again");
+    }
+  }
+};
+
+document
+  .querySelector('.login-form')
+  .addEventListener('submit', loginFormHandler);
+
+document
+  .querySelector('.signup-form')
+  .addEventListener('submit', signupFormHandler);
+
+
+const loginForm = document.getElementById("loginForm")
+const signUpForm = document.getElementById("signUpForm")
+const signUpSwitch = document.getElementById("signUpBtn")
+const loginSwitch = document.getElementById("loginBtn")
+
+signUpSwitch.addEventListener("click", function() {
+  loginForm.style.display = "none"
+  signUpForm.style.display = "block"
+})
+
+loginSwitch.addEventListener("click", function() {
+  loginForm.style.display = "block"
+  signUpForm.style.display = "none"
+})
